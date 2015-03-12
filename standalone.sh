928bcfc3d4e9c5549ab923b7fdd240816f1b5eed
supersheep <supersheepxjy@gmail.com>
2015-03-12 19:38:40 +0800

    update standalone.sh

--- standalone.sh
+++ standalone.sh
@@ -1,14 +1,14 @@
-# if [ -z "$ENV" ]; then
+if [ -z "$ENV" ]; then
 	echo "cortex bundle -o ./dest/standalone.js"
 	cortex bundle -o ./dest/standalone.js
-# 	exit 0
-# fi
+	exit 0
+fi
 
-# cortex bundle --help > /dev/null
-# if [ "$?" = "1" ]; then
-# 	echo "npm install cortex-bundle -g"
-# 	npm install cortex-bundle -g
-# fi
+cortex bundle --help > /dev/null
+if [ "$?" = "1" ]; then
+	echo "npm install cortex-bundle -g"
+	npm install cortex-bundle -g
+fi
 
-# echo "cortex bundle --built-root $CORTEX_DEST --prerelease $ENV -o $CORTEX_DEST/$CORTEX_PACKAGE_NAME/$CORTEX_PACKAGE_VERSION/standalone.js"
-# cortex bundle --built-root $CORTEX_DEST --prerelease $ENV -o $CORTEX_DEST/$CORTEX_PACKAGE_NAME/$CORTEX_PACKAGE_VERSION/standalone.js
+echo "cortex bundle --built-root $CORTEX_DEST --prerelease $ENV -o $CORTEX_DEST/$CORTEX_PACKAGE_NAME/$CORTEX_PACKAGE_VERSION/standalone.js"
+cortex bundle --built-root $CORTEX_DEST --prerelease $ENV -o $CORTEX_DEST/$CORTEX_PACKAGE_NAME/$CORTEX_PACKAGE_VERSION/standalone.js
