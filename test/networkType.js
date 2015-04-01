8e30c94d54c50c90ca98f792305d274fb114db72
supersheep <supersheepxjy@gmail.com>
2015-04-01 16:09:10 +0800

    add test cases for network

--- test/networkType.js
+++ test/networkType.js
@@ -1,9 +1,59 @@
+var assert = require('assert');
 var Patch7_1 = require('../lib/patch-7.1');
 
+describe("description", function(){
+  it("ios should be ok", function(){
+    // your code...
+		assert.equal(Patch7_1._iOSNetworkType({
+			type: "0",
+			subType: ""
+		}),"none");
 
-var result = Patch7_1._iOSNetworkType({
-	type: 327683,
-	subType: "CTRadioAccessTechnologyEdge"
+		assert.equal(Patch7_1._iOSNetworkType({
+			type: "85538",
+			subType: ""
+		}),"wifi");
+
+		assert.equal(Patch7_1._iOSNetworkType({
+			type: "327683",
+			subType: "CTRadioAccessTechnologyEdge"
+		}),"2g");
+
+		assert.equal(Patch7_1._iOSNetworkType({
+			type: "327683",
+			subType: "CTRadioAccessTechnologyHSDPA"
+		}),"3g");
+
+		assert.equal(Patch7_1._iOSNetworkType({
+			type: "327683",
+			subType: "CTRadioAccessTechnologyLTE"
+		}),"4g");
+  });
+
+
+  it("android should be ok", function(){
+
+		assert.equal(Patch7_1._androidNetworkType({
+		}),"none");
+
+		assert.equal(Patch7_1._androidNetworkType({
+			type: 1,
+			subType: 0
+		}),"wifi");
+
+		assert.equal(Patch7_1._androidNetworkType({
+			type: 0,
+			subType: 4
+		}),"2g");
+
+		assert.equal(Patch7_1._androidNetworkType({
+			type: 0,
+			subType: 8
+		}),"3g");
+
+		assert.equal(Patch7_1._androidNetworkType({
+			type: 0,
+			subType: 13
+		}),"4g");
+  });
 });
-
-console.log(result);
