2cc61504ce52ea17f002b9c6426bbbdfc662e6a2
supersheep <supersheepxjy@gmail.com>
2015-04-02 17:16:04 +0800

    Site updated to 8e30c94

--- README.md
+++
@@ -1,3 +0,0 @@
-# DPApp.js
-
-文档： [http://efte.github.io/dpapp/](efte.github.io/dpapp/)
--- build-demo.sh
+++
@@ -1,4 +0,0 @@
-cortex build
-export ENV=beta
-cortex bundle -o ./demo/standalone.js --path http://i{n}.dpfile.com/mod/
-gulp ftp
