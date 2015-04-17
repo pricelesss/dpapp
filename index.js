f6aa0ccc169c55fe43d3fe75f11a7352cdab12fc
supersheep <supersheepxjy@gmail.com>
2015-04-17 12:27:03 +0800

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
   var DPAppNativeCore = require('./lib/native-core');
   var decorator = require('./lib/decorator');
   var decorateForTrace = function(target){
-    window.DPApp = decorator(target);
+    Host.DPApp = target = decorator(target);
   };
   require('./lib/errortrace');
   // Require different platform js base on userAgent.
@@ -22,38 +22,38 @@
   }
 
   if (DPAppNativeCore._uaVersion == "7.1.x") {
-    DPApp = DPAppNativeCore.extend(require('./lib/patch-7.1'));
-    decorateForTrace(DPApp);
+    _DPApp = DPAppNativeCore.extend(require('./lib/patch-7.1'));
+    decorateForTrace(_DPApp);
   } else if(DPAppNativeCore._uaVersion == "7.0.x"){
     // 目前有修改UA，而api尚未对齐的仅7.0版本
-    DPApp = DPAppNativeCore.extend(require('./lib/patch-7.0'));
-    decorateForTrace(DPApp);
+    _DPApp = DPAppNativeCore.extend(require('./lib/patch-7.0'));
+    decorateForTrace(_DPApp);
   } else{
     var patch6 = require('./lib/patch-6.x');
     var web = require('./lib/web');
     // 默认认为6.x，当接口调用失败，认为是web
-    DPApp = DPAppNativeCore.extend(patch6);
-    DPApp._patch6Ready = DPApp.ready;
-    DPApp.ready = function(callback){
+    _DPApp = DPAppNativeCore.extend(patch6);
+    _DPApp._patch6Ready = _DPApp.ready;
+    _DPApp.ready = function(callback){
       var cfg = DPApp._cfg;
       var timeout = setTimeout(function(){
-        DPApp._bindDOMReady(function(){
+        _DPApp._bindDOMReady(function(){
           web._cfg = cfg;
           web._isReady = DPApp._isReady;
           decorateForTrace(web);
           callback();
         });
       }, 50);
-      DPApp._patch6Ready(function(){
+      _DPApp._patch6Ready(function(){
         clearTimeout(timeout);
         callback();
       });
-      decorateForTrace(DPApp);
+      decorateForTrace(_DPApp);
     }
-    decorateForTrace(DPApp);
+    decorateForTrace(_DPApp);
   }
 
-  DPApp.getQuery = getQuery;
+  _DPApp.getQuery = getQuery;
 
 
   // Export DPApp object, if support AMD, CMD, CommonJS.
@@ -64,7 +64,7 @@
   // Export DPApp object to Host
   if (typeof Host !== 'undefined') {
     if(Host.DPApp){
-      DPApp._mixin(Host.DPApp, _DPApp);
+      _DPApp._mixin(Host.DPApp, _DPApp);
     }else{
       Host.DPApp = _DPApp;
     }
